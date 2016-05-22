d94ce056c2c0ffda21504020837ed4b278920120
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-05-22 23:46:44 +0200

    Use only lower 16 bit of pid to get uniq id.

--- ping_mtu.py
+++ ping_mtu.py
@@ -11,7 +11,7 @@ srcaddr=sys.argv[1]
 dstaddr=sys.argv[2]
 size=int(sys.argv[3])
 expect=int(sys.argv[4])
-pid=os.getpid()
+pid=os.getpid() & 0xffff
 hdr=IP(flags="DF", src=srcaddr, dst=dstaddr)/ICMP(id=pid)
 payload="a" * (size - len(str(hdr)))
 ip=hdr/payload
